2c491daf9464c829949097d71f1a3b6954779b18
mutsuko <mymy11853@gmail.com>
2019-03-07 15:45:55 +0900

    [WID]メッセージ送信機能実装
    ## what
    Chatspaceのメッセージ送信機能
    出来ていなかった箇所を修正しました

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,7 +10,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
-      @messages = @group.messages.includes(:user)
+      @message = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください'
       render :index
     end
