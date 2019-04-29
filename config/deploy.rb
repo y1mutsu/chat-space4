074d436670f9a928c070d8ff6bf4aceb3f9cc55e
mutsuko <mymy11853@gmail.com>
2019-04-30 08:13:31 +0900

    デプロイ２

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,5 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
-
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
 set :application, 'chat-space4'
 set :repo_url,  'git@github.com:y1mutsu/chat-space4.git'
 
@@ -12,7 +9,8 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/yoshimuramutsukotoko.pem ec2-user@13.115.10.120']
+                  keys: ['~/.ssh/yoshimuramutsukotoko.pem']
+
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
